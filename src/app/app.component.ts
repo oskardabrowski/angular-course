666fdc09157c8ac7b37c4c38bda02560cb3d0b56
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2022-07-13 21:56:42 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  intervalSub: NodeJS.Timer | undefined;
+
+  ngOnInit(): void {
+    this.intervalSub = setInterval(() => {
+      console.log('Hello from ngOnInit');
+    }, 1000)
+  }
+
+  ngOnDestroy(): void {
+    if(this.intervalSub) {
+      clearInterval(this.intervalSub)
+    }
+  }
+
   title = 'angular-tutorial';
 }
