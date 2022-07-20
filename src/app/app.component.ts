ca7cafd579d2e21d2dd97d1be44cf9486ac68c2f
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2022-07-21 01:33:58 +0200

    1h of tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,120 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <p [appHighlight]="'red'">Test</p>
+  `
 })
-export class AppComponent {
-  title = 'Hello world!';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
 
-  getMin(a: number,b: number) {
-    if(a < b) {
-      return a;
-    }
-
-    return b;
   }
 }
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//   <label for="example">{{name}}</label>
+//   <input [(ngModel)]="name" id="example" />
+//   `
+// })
+// export class AppComponent implements OnInit {
+
+//   isSpecial = true;
+//   currentClasses = {};
+//   currentStyle = {};
+//   name = '';
+
+//   ngOnInit() {
+//     this.setCurrentClasses();
+//     this.setCurrentStyle();
+//   }
+
+//   setCurrentClasses() {
+//     this.currentClasses = {
+//       savable: true,
+//       modified: false,
+//       special: true
+//     }
+//   }
+
+//   setCurrentStyle() {
+//     this.currentStyle = {
+//       'font-style': 'italic',
+//       'font-weight': 'bold'
+//     }
+//   }
+// }
+
+  // template: `<div [ngClass]="isSpecial ? 'special' : ''">Example</div>`
+
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//   <input #ref1 type="text" [(ngModel)]="firstExample" />
+//   <span *ngIf="true">{{ref1.value}}</span>
+//   `,
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   firstExample = '';
+// }
+// @Component({
+//   selector: 'app-root',
+//   template: `<input #phone placeholder="phone" />
+//   <button (click)="callPhone(phone.value)">Call</button>`,
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   callPhone(phone: string) {
+//     alert(phone)
+//   }
+// }
+// export class AppComponent {
+//   fontSizepx = 16;
+// }
+// export class AppComponent {
+//   deleteItem(item: string) {
+//     alert(item)
+//   }
+// }
+// export class AppComponent {
+//   itemImageUrl = '../assets/neil-soni-6wdRuK7bVTE-unsplash.jpg';
+//   isUnchanged = true;
+// }
+// export class AppComponent {
+//   itemImageUrl = '../assets/neil-soni-6wdRuK7bVTE-unsplash.jpg';
+//   isUnchanged = true;
+// }
+// export class AppComponent {
+//   todayDate = new Date();
+// }
+
+
+// export class AppComponent {
+//   showText = false;
+
+//   toggleText(event: any): void {
+//     this.showText = !this.showText;
+//     console.log(event);
+//   }
+// }
+
+
+
+// export class AppComponent {
+//   title = 'Hello world!';
+
+//   getMin(a: number,b: number) {
+//     if(a < b) {
+//       return a;
+//     }
+
+//     return b;
+//   }
+// }
 
 // ! Hooki
 // import { Component, OnInit, OnDestroy } from '@angular/core';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,26 @@ import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
+import { GreetingsPipePipe } from './greetings-pipe.pipe';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { AppSizerComponent } from './app-sizer/app-sizer.component';
+import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from './hightlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
     ParentComponent,
-    ChildComponent
+    ChildComponent,
+    GreetingsPipePipe,
+    ItemDetailsComponent,
+    AppSizerComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
